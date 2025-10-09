33a29b4fb292e523c2a83b27c2524d10db7fe801
Thomas Ferrandiz <thomas.ferrandiz@suse.com>
2025-10-09 15:04:50 +0000

    add support for starting flannel with the nftables backend

--- pkg/agent/flannel/flannel.go
+++ pkg/agent/flannel/flannel.go
@@ -27,7 +27,9 @@ import (
 	"github.com/flannel-io/flannel/pkg/backend"
 	"github.com/flannel-io/flannel/pkg/ip"
 	"github.com/flannel-io/flannel/pkg/subnet/kube"
+	"github.com/flannel-io/flannel/pkg/trafficmngr"
 	"github.com/flannel-io/flannel/pkg/trafficmngr/iptables"
+	"github.com/flannel-io/flannel/pkg/trafficmngr/nftables"
 	"github.com/joho/godotenv"
 	pkgerrors "github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -84,7 +86,17 @@ func flannel(ctx context.Context, wg *sync.WaitGroup, flannelIface *net.Interfac
 	if err != nil {
 		return pkgerrors.WithMessage(err, "failed to register flannel network")
 	}
-	trafficMngr := &iptables.IPTablesManager{}
+
+	// Instanciate a TrafficManager to clean-up the rules of the backend we don't use
+	// This is to ensure a clean state in case flannel is restarted with a different choice
+	cleanupMngr := newTrafficManager(!config.EnableNFTables)
+	err = cleanupMngr.CleanUp(ctx)
+	if err != nil {
+		return pkgerrors.WithMessage(err, "failed to clean-up iptables/nftables rules")
+	}
+
+	//Create TrafficManager and instantiate it based on whether we use iptables or nftables
+	trafficMngr := newTrafficManager(config.EnableNFTables)
 	err = trafficMngr.Init(ctx)
 	if err != nil {
 		return pkgerrors.WithMessage(err, "failed to initialize flannel ipTables manager")
@@ -286,4 +298,12 @@ func ReadIP6CIDRsFromSubnetFile(path string, CIDRKey string) []ip.IP6Net {
 		}
 	}
 	return prevCIDRs
+}
+
+func newTrafficManager(useNftables bool) trafficmngr.TrafficManager {
+	if useNftables {
+		return &nftables.NFTablesManager{}
+	} else {
+		return &iptables.IPTablesManager{}
+	}
 }
